e9c4cd3b2d613520bd6bf054ac2b353f853c657d
James Dooley <james.dooley@bbc.co.uk>
2018-09-19 16:18:54 +0100

    Fixing opts conflict

--- example-usage.js
+++ example-usage.js
@@ -3,11 +3,9 @@ const tweetThatClip = require('./index.js');
 const opts = {
   inputFile: './assets/test.mp4',
   outputFile: './assets/test_clipped.mp4',
-  inputSeconds: 300,
-  durationSeconds: 5,
-  tweetText: 'This is the accompanying text with the video',
-  // optional
-  ffmpegPath: "/usr/local/bin/ffmpeg",
+  inputSeconds: 405,
+  durationSeconds: 15,
+  tweetText: 'This is the accompanying text with the video'
 };
 
 tweetThatClip(opts, (err, res) => {
