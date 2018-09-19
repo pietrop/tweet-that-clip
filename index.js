f8394b1d67622bc460636fbd108e05c833bbcfdc
James Dooley <james.dooley@bbc.co.uk>
2018-09-19 14:06:15 +0100

    Adding twitter media upload and chaining with ffmpeg trim

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const trim = require('./lib/trim');
+const upload = require('./lib/upload');
 
-const inputFile = './assets/test.mp4';
-const outputFile = './assets/test_clipped.mp4';
-const inputSeconds = 200;
-const durationSeconds = 5;
+function tweetThatClip() {
+  const inputFile = './assets/test.mp4';
+  const outputFile = './assets/test_clipped.mp4';
+  const inputSeconds = 300;
+  const durationSeconds = 5;
+  const tweetText = 'This is the accompanying text with the video';
 
-trim(inputFile, inputSeconds, durationSeconds, outputFile, (err, res) => {
-  if (err) console.log(err);
+  trim(inputFile, inputSeconds, durationSeconds, outputFile, (err, res) => {
+    if (err) console.log(err);
+    console.log('Transcoding finished.');
 
-  console.log('Transcoding finished.');
-});
+    upload(tweetText, outputFile, (err, res) => {
+      if (err) console.log(err);
+      console.log('Upload finished.');
+    });
+  });
+};
+
+tweetThatClip();
