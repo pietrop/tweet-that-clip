06142ba5e10b4c370ad37da4e613fa3733159a26
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2018-09-25 22:23:02 +0100

    re-introduced callback
    I had accidentally removed the callback in the new tweet with video module, re-introduced it, and increases version number to publish to npm

--- example-usage.js
+++ example-usage.js
@@ -5,7 +5,7 @@ const opts = {
   inputFile: path.join(__dirname,'./assets/test.mp4'),
   outputFile: path.join(__dirname,'./assets/test_clipped.mp4'),
   inputSeconds: 10, // seconds
-  durationSeconds: 120, // in seconds. Up to 2min duration 
+  durationSeconds: 10, // in seconds. Up to 2min duration 
   // Twitter text status  280 characters limit.
   tweetText: 'The Trussell Trust found that food bank use increased by 52% in a year in areas where Universal Credit has been rolled out. The National Audit Office observed similar findings https://fullfact.org/economy/universal-credit-driving-people-food-banks/'//, 
   // optional ffmpeg path
@@ -19,5 +19,6 @@ const opts = {
 };
 
 tweetThatClip(opts, (err, res) => {
+  console.log('in example-usage');
   console.log(err, res);
 });
