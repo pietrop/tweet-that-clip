bf782c79646ff55d9ea93960f0d0bdb6d3a5b258
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2018-10-21 22:41:09 +0100

    added support for audio and captions
    added support for burning captions for both audio and video. Added support to upload video with animated wave form

---
+++ example-usage-audio.js
@@ -0,0 +1,33 @@
+/**
+ *  Example audio usage, with with captions
+ */
+const tweetThatClip = require('./index.js');
+const path = require('path');
+
+const opts = {
+  inputFile: path.join(__dirname,'./assets/test.mp3'),
+  mediaType: 'audio', // 'audio' or 'video'
+  outputFile: path.join(__dirname,'/example/test_clipped.mp4'),
+  inputSeconds: 10, // seconds
+  durationSeconds: 20, // in seconds. Up to 2min duration 
+  // Twitter text status  280 characters limit.
+  tweetText: 'The Trussell Trust found that food bank use increased by 52% in a year in areas where Universal Credit has been rolled out. The National Audit Office observed similar findings https://fullfact.org/economy/universal-credit-driving-people-food-banks/', 
+  // optional path to ffmpeg. eg To burn captions, needs, optional path to ffmpeg binary - enable libas, 
+  // if not provided it uses default on system if present
+  // if in doubt can give the path to https://www.npmjs.com/package/ffmpeg-static-electron
+  // ffmpegPath: "/usr/local/bin/ffmpeg",
+  ffmpegPath:'/Users/passap02/autoEdit_2/node_modules/ffmpeg-static-electron/bin/mac/x64/ffmpeg',
+  // Optional caption file - if burning captions provide an srtFilePath.
+  srtFilePath:  path.join(__dirname,'./assets/captions.srt')
+  // credentials: {
+  //   consumerKey: "",
+  //   consumerSecret: "",
+  //   accessToken: "",
+  //   accessTokenSecret: ""
+  // }
+};
+
+tweetThatClip(opts, (err, res) => {
+  console.log('in example-usage for audio');
+  // console.log(err, res);
+});
--- index.js
+++ index.js
@@ -4,17 +4,123 @@
  * see specs 
  * https://developer.twitter.com/en/docs/media/upload-media/uploading-media/media-best-practices
  */
+const path = require('path');
 const fs = require('fs');
 const trimVideo = require('./lib/trim/index.js');
+const convertToTwitterVideoSpecs = require('./lib/convert-to-twitter-video-specs/index.js')
+const audioToVideoWaveForm = require('./lib/audio-to-video-waveform/index.js');
+const burnCaptions  = require('./lib/burn-captions/index.js');
 const TweetWithVideo = require('./lib/TweetWithVideo/index.js');
 
+// TODO: needs refactoring and simplifying 
 function tweetThatClip(opts, callback) {
-  trimVideo(opts, (err, res) => {
-    if (err) return callback(err);
-    console.log('Twitter Transcoding finished.');
-  
+  let fileExtension = path.extname(opts.outputFile);
+  let tmpOutputForTrim = `${opts.inputFile}.trimmed${fileExtension}`;
+  let tmpOutputForWave = `${opts.outputFile}.wave${fileExtension}`;
+  let tmpOutputForBunt = `${opts.outputFile}.burnt${fileExtension}`;
+  // if there is no SRT file, then skip burning captions for audio
+  if(opts.srtFilePath === undefined && opts.mediaType ==='audio'){
+    tmpOutputForBunt = opts.outputFile;
+  }
+  // if there is no SRT file, then skip burning captions for video
+  if(opts.srtFilePath === undefined && opts.mediaType ==='video'){
+    tmpOutputForTrim = opts.outputFile;
+  }
+
+  trimVideo({
+    inputFile: opts.inputFile,
+    outputFile: tmpOutputForTrim,
+    inputSeconds: opts.inputSeconds,
+    durationSeconds: opts.durationSeconds
+  }, (err, res) => {
+    // if (err) return callback(err);
+    console.log('Twitter Trimming finished.');
+        // if it's audio convert to video by adding wave form 
+        if(opts.mediaType === 'audio'){
+          console.log('is audio')
+          audioToVideoWaveForm({
+            audioSrc: tmpOutputForTrim,
+            outpuFileName: tmpOutputForWave
+            // TODO: should refactor this to return a promise?
+          }, (res)=>{
+              console.log('Done creating audio to video waveform');
+              convertToTwitterVideoSpecs({
+                inputFile: tmpOutputForWave,
+                outputFile: tmpOutputForBunt
+                // outputFile: '/Users/passap02/tweet-that-clip/assets/test_burnt.mp4'
+              },(error, res)=>{
+                console.log('Done converting to twitter video specs ', tmpOutputForBunt);
+                // do something with output file
+                console.log(res)
+                if(opts.srtFilePath!== undefined){
+                  burnCaptions({
+                  // videoSrc: opts.outputFile+'.twitterspec'+fileExtension,
+                  videoSrc: tmpOutputForBunt,
+                  srtFilePath: opts.srtFilePath,
+                  outputFile:  opts.outputFile,
+                  ffmpegPath: opts.ffmpegPath
+                  }, (res)=>{
+                      console.log('Done burning captions', opts.outputFile);
+                      // do something with result
+                      console.log(res);
+                      tweetClipHelper(opts, (error, res)=>{
+                        console.log(error, res)
+                          if(callback){callback(null,res)};
+                      })
+                  })
+                }else{
+                  tweetClipHelper(opts, (error, res)=>{
+                    console.log(error, res)
+                    if(callback){callback(null,res)};
+                  })
+                }
+              })
+              
+          });
+        } 
+        // otherwise if video continue and add captions - ? optional for now
+        else if(opts.mediaType ==='video'){
+          console.log('is video')
+          if(opts.srtFilePath!== undefined){
+            burnCaptions({
+            // videoSrc: opts.outputFile+'.twitterspec'+fileExtension,
+              videoSrc: tmpOutputForTrim,
+              srtFilePath: opts.srtFilePath,
+              outputFile:  opts.outputFile,//  __dirname+'/test-burnt.mp4'
+              ffmpegPath: opts.ffmpegPath
+            }, (res)=>{
+              console.log('Done burning captions', opts.outputFile);
+                // do something with result
+                console.log(res);
+                tweetClipHelper(opts, (error, res)=>{
+                  console.log(error, res)
+                  if(callback){callback(null,res)};
+                })
+            })
+          } else{
+            tweetClipHelper(opts, (error, res)=>{
+              console.log(error, res);
+              if(callback){callback(null,res)};
+            })
+          }
+        }
+  });
+
+  /**
+   * Helper function to remove tmp files used to create segment
+   * to upload to twitter
+   * @param {string} final  - expects path to final clip sent to twitter
+   */
+  function cleanUpTmpFiles(final){
+    ifPresentDeleteFile(tmpOutputForTrim);
+    ifPresentDeleteFile(tmpOutputForWave);
+    ifPresentDeleteFile(tmpOutputForBunt);
+    ifPresentDeleteFile(final);
+  }
+
+  function tweetClipHelper(opts, callback){
     // TODO: this could be refactored? to be a bit cleaner
-    videoTweet = new TweetWithVideo({
+    let videoTweet = new TweetWithVideo({
       // if they are not set is just passed as undefined. for now keeping logic of deciding which credentials to use use within the module, while figure out cleaner solution.
       credentials: opts.credentials,
       file_path: opts.outputFile,
@@ -22,8 +128,9 @@ function tweetThatClip(opts, callback) {
     }, (error, response)=>{
       // Deleting the trimmed clip 
       console.log(opts.outputFile);
-      fs.unlinkSync(opts.outputFile);
+      // fs.unlinkSync(opts.outputFile);
       console.info('Twitter upload finished.');
+      cleanUpTmpFiles(opts.outputFile);
       if (error) {
         return callback(error,null);
       }
@@ -31,8 +138,14 @@ function tweetThatClip(opts, callback) {
         return  callback(null, response);
       }
     });
+  }
 
-  });
 };
+
+function ifPresentDeleteFile(path){
+  if (fs.existsSync(path)) {
+    fs.unlinkSync(path);
+  }
+}
 
 module.exports = tweetThatClip;
